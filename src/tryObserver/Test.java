58e66fcdcd642e5ec71edd08e6a50910e71c152b
DemiwizAces <erdmriosnadeau@gmail.com>
2023-09-12 11:49:17 -0400

    Initial Commit

---
+++ src/tryObserver/Test.java
@@ -0,0 +1,16 @@
+package tryObserver;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<Customer> customerList = new ArrayList<Customer>();
+		ArrayList<Store> storeList = new ArrayList<Store>();
+		for(int x = 0; x < 20; x++) {
+			storeList.add();
+			
+		}
+	}
+
+}
